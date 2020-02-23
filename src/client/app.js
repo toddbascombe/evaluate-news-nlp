669462e73610fd96af3958124c89ef1d78305bd9
todd bascombe <tech@ccnyalumni.com>
2020-02-22 20:22:53 -0500

    aylient sdk is working

--- src/client/app.js
+++ src/client/app.js
@@ -6,22 +6,35 @@ const submit_user_value = (event) => {
     event.preventDefault();
     const value = user_input.value
     console.log(value)
-    postData("/", { "value": value })
+    postData("/", { "value": value });
+    server_data()
 }
 
 
 const postData = async(url = "", data = {}) => {
-    await fetch(url, {
-            method: "POST",
-            mode: 'cors',
-            cache: 'no-cache',
-            credentials: 'same-origin',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        }).then(res => res.text()) // convert to plain text
-        .then(text => console.log(text));
+    const response = await fetch(url, {
+        method: "POST",
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+    try {
+        server_data()
+    } catch (err) {
+        console.log(err)
+    };
+}
+
+
+
+const server_data = async() => {
+    await fetch("/aidata").then(value => {
+        console.log(value)
+    })
 }
 
 submit_btn.addEventListener("click", submit_user_value);
--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
+require('dotenv').config()
 const express = require("express");
 const app = express();
 const port = 8080;
 const cors = require('cors');
 const bodyParser = require("body-parser")
+const AYLIENTextAPI = require('aylien_textapi');
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -10,13 +12,39 @@ app.use(bodyParser.json());
 
 app.use(express.static("client"));
 
+const projectData = {};
+
+
+const textapi = new AYLIENTextAPI({
+    application_id: process.env.apiId,
+    application_key: process.env.apiKey
+});
+
+
+
+
 
 app.get("/", (req, res) => {
     res.send('index.html');
 });
 
-app.post("/", (req, res) => {
-    console.log("done")
+app.post("/", async(req, res) => {
+    const data = req.body.value
+    await textapi.sentiment({
+        url: data,
+        mode: 'document'
+    }, function(error, response) {
+        if (error === null) {
+            projectData.ai_info = response;
+            return response
+        }
+    });
+
+});
+
+app.get("/aidata", (req, res) => {
+    console.log(projectData.ai_info)
+    res.send([projectData])
 })
 
 app.listen(port, () => {
