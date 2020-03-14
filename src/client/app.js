14472ac3287c8caa1ec059d8248d1c188c3587c5
tech@ccnyalumni.org <tech@ccnyalumni.org>
2020-03-14 14:19:24 -0400

    AYLIENTextAPI is woking

--- src/client/app.js
+++ src/client/app.js
@@ -1,40 +1,36 @@
 const user_input = document.querySelector("#article");
 const submit_btn = document.querySelector("#submit");
 
+const submit_user_value = event => {
+  event.preventDefault();
+  const value = user_input.value;
+  console.log(value);
+  postData("/", { value: value });
+  server_data();
+};
 
-const submit_user_value = (event) => {
-    event.preventDefault();
-    const value = user_input.value
-    console.log(value)
-    postData("/", { "value": value });
-    server_data()
-}
+const postData = async (url = "", data = {}) => {
+  const response = await fetch(url, {
+    method: "POST",
+    mode: "cors",
+    cache: "no-cache",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  });
+  try {
+    console.log(response);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-
-const postData = async(url = "", data = {}) => {
-    const response = await fetch(url, {
-        method: "POST",
-        mode: 'cors',
-        cache: 'no-cache',
-        credentials: 'same-origin',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    });
-    try {
-        server_data()
-    } catch (err) {
-        console.log(err)
-    };
-}
-
-
-
-const server_data = async() => {
-    await fetch("/aidata").then(value => {
-        console.log(value)
-    })
-}
+const server_data = async () => {
+  await fetch("/data").then(value => {
+    console.log(value);
+  });
+};
 
 submit_btn.addEventListener("click", submit_user_value);
--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = 8080;
-const cors = require('cors');
-const bodyParser = require("body-parser")
-const AYLIENTextAPI = require('aylien_textapi');
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const AYLIENTextAPI = require("aylien_textapi");
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -14,39 +14,36 @@ app.use(express.static("client"));
 
 const projectData = {};
 
-
 const textapi = new AYLIENTextAPI({
-    application_id: process.env.apiId,
-    application_key: process.env.apiKey
+  application_id: process.env.apiId,
+  application_key: process.env.apiKey
 });
-
-
-
-
 
 app.get("/", (req, res) => {
-    res.send('index.html');
+  res.send("index.html");
 });
 
-app.post("/", async(req, res) => {
-    const data = req.body.value
-    await textapi.sentiment({
-        url: data,
-        mode: 'document'
-    }, function(error, response) {
-        if (error === null) {
-            projectData.ai_info = response;
-            return response
-        }
-    });
-
+app.post("/", async (req, res) => {
+  const data = req.body.value;
+  await textapi.sentiment(
+    {
+      url: data,
+      mode: "document"
+    },
+    function(error, response) {
+      if (error === null) {
+        projectData.ai_info = response;
+        return response;
+      }
+    }
+  );
 });
 
-app.get("/aidata", (req, res) => {
-    console.log(projectData.ai_info)
-    res.send([projectData])
-})
+app.get("/data", (req, res) => {
+  console.log(projectData.ai_info);
+  res.send([projectData]);
+});
 
 app.listen(port, () => {
-    console.log("The Server is Up and running")
-})
+  console.log("The Server is Up and running");
+});
